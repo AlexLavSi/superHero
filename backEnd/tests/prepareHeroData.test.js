d0a374058934ffca90cc503ed5c1b5ec87c2197e
a.levitskiy <a.levitski@hqsoftwarelab.com>
2020-07-16 21:41:06 +0300

    added first test

---
+++ backEnd/tests/prepareHeroData.test.js
@@ -0,0 +1,27 @@
+const {expect} = require("chai");
+const prepareHeroData = require("../app/helpers/prepareHeroData").prepareHeroData;
+
+describe('positive', function () {
+  it('return Hero Data', function () {
+    const request = {
+      id: 1,
+      intelligence: 38,
+      strength: 100,
+      speed: 17,
+      durability: 80,
+      power: 24,
+      combat: 64,
+      img: 'https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg',
+      biography: '',
+    };
+
+    const response = {
+      id: 1,
+      img: 'https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg',
+      biography: '',
+      powerstats: { intelligence: 38, strength: 100, speed: 17, durability: 80, power: 24, combat: 64 }
+    };
+
+    expect(prepareHeroData([request])).to.deep.include(response);
+  });
+})
